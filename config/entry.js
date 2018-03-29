c516df8b11dfa76909bb70f2c2c9a124099b7632
IVLIU <IVLIU.github.com>
2018-03-29 18:40:35 +0800

    extract

--- config/entry.js
+++ config/entry.js
@@ -1,11 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function() {
-    let entries = {};
-    let files = fs.readdirSync('./src/scripts');
-    files.forEach(file => {
-        entries[path.basename(file, '.js')] = `./src/scripts/${file}`
-    })
-    return entries;
+// module.exports = function() {
+//     let entries = {};
+//     let files = fs.readdirSync('./src/scripts');
+//     files.forEach(file => {
+//         entries[path.basename(file, '.js')] = `./src/scripts/${file}`
+//     })
+//     return entries;
+// }
+
+module.exports = {
+    entriesJs: function() {
+        let entriesJs = {};
+        let files = fs.readdirSync('./src/scripts');
+        files.forEach(file => {
+            entriesJs[path.basename(file, '.js')] = `./src/scripts/${file}`
+        })
+        return entriesJs;
+    },
+    entriesCss: function() {
+        let entriesCss = {};
+        let files = fs.readdirSync('./src/styles');
+        files.forEach(file => {
+            entriesCss[path.basename(file, '.css')] = `./src/styles/${file}`;
+        })
+        return entriesCss;
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
-const entries = require('./config/entry');
+const { entriesJs, entriesCss } = require('./config/entry');
 
 module.exports = {
     entry: {
-        ...entries()
+        ...entriesJs(), ...entriesCss()
     },
     output: {
-        path: path.join(__dirname, 'dist/scripts'),
-        filename: '[name].[hash].js'
-    }
+        path: path.join(__dirname, 'dist'),
+        filename: 'scripts/[name].[hash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin({
+            filename: 'styles/[name].[hash].css' //放到dist/css/下
+        }),
+    ]
 }
